e5c3533a5ca39f904fd997e037f67f9a82897990
artofnext <novnovalex@gmail.com>
2021-07-14 18:15:45 +0100

    add scss to component

--- src/components/ExpenceItem.js
+++ src/components/ExpenceItem.js
@@ -1,5 +1,15 @@
+import "./ExpenceItem.scss";
+
 function ExpenceItem() {
-    return <h2>Expence Item!</h2>
+    return (
+        <div className="expense-item">
+            <div>March</div>
+            <div className="expense-item__description">
+                <h2>Car</h2>
+                <div className="expense-item__price">$200</div>
+            </div>
+        </div>
+    );
 }
 
-export default ExpenceItem
+export default ExpenceItem;
