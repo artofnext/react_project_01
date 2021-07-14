b167e20314ec6fde6c29383c1d9c730a4726356b
artofnext <novnovalex@gmail.com>
2021-07-14 16:20:22 +0100

    create component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenceItem from "./components/ExpenceItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>Some thing</p>
+      <ExpenceItem></ExpenceItem>
     </div>
   );
 }
