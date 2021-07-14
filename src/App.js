0ac87d2d86a7d7798f3e38f2fdbe29dc432229ad
artofnext <novnovalex@gmail.com>
2021-07-14 19:34:56 +0100

    refactor - rename

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     return (
         <div>
             <h2>Let's get started!</h2>
-            <Expenses expenses={expenses} />
+            <Expenses items={expenses} />
         </div>
     );
 }
