2dfa8c4620d551676b13a58ebfccc9090e7bf7e7
artofnext <novnovalex@gmail.com>
2021-07-15 14:55:40 +0100

    use states and events

--- src/components/Expences/ExpenceItem.js
+++ src/components/Expences/ExpenceItem.js
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenceItem.scss";
 import ExpenceDate from "../Expences/ExpenceDate";
 import Card from "../UI/Card"
 
 const ExpenceItem = (props) => {
+    // use array destructuring since useState returns an array of 2 elements
+    const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        setTitle('Updated!')
+        console.log(title)
+    }
     return (
         <Card className="expense-item">
             <ExpenceDate date={props.date}></ExpenceDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Update</button>
         </Card>
     );
 }
