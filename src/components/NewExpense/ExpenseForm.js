146041722c1cb01bb47e29f2c1662f8d7701fbcf
artofnext <novnovalex@gmail.com>
2021-07-15 15:05:04 +0100

    one state or multiple states approach

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,51 +1,71 @@
-import React, { useState } from 'react';
-import './ExpenseForm.scss';
+import React, { useState } from "react";
+import "./ExpenseForm.scss";
 
 const ExpenseForm = () => {
-    const [inputTitle, setInputTitle] = useState('');
-    const [inputAmount, setInputAmount] = useState('');
-    const [inputDate, setInputDate] = useState('');
+    // separate states
+    // const [inputTitle, setInputTitle] = useState('');
+    // const [inputAmount, setInputAmount] = useState('');
+    // const [inputDate, setInputDate] = useState('');
+
+    // one state with object
+    const [userInput, setUserInput] = useState({
+        inputTitle: "",
+        inputAmount: "",
+        inputDate: "",
+    });
 
     const titleChangeHandler = (event) => {
-        setInputTitle(event.target.value);
+        // setInputTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            inputTitle: event.target.value,
+        });
     };
 
     const amountChangeHandler = (event) => {
-        setInputAmount(event.target.value);
+        // setInputAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            inputAmount: event.target.value,
+        });
     };
 
     const dateChangeHandler = (event) => {
-        setInputDate(event.target.value);
+        // setInputDate(event.target.value);
+        setUserInput({
+            ...userInput,
+            inputData: event.target.value,
+        })
     };
 
     return (
         <form>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
-                <div className='new-expense__control'>
+                <div className="new-expense__control">
                     <label>Amount</label>
                     <input
-                        type='number'
-                        min='0.01'
-                        step='0.01'
+                        type="number"
+                        min="0.01"
+                        step="0.01"
                         onChange={amountChangeHandler}
                     />
                 </div>
-                <div className='new-expense__control'>
+                <div className="new-expense__control">
                     <label>Date</label>
                     <input
-                        type='date'
-                        min='2019-01-01'
-                        max='2022-12-31'
+                        type="date"
+                        min="2019-01-01"
+                        max="2022-12-31"
                         onChange={dateChangeHandler}
                     />
                 </div>
             </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
             </div>
         </form>
     );
