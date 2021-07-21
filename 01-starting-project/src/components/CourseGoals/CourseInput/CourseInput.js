703996131413418f805012e061b079ad3c0be559
artofnext <novnovalex@gmail.com>
2021-07-21 17:18:29 +0100

    scope css using modules

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,44 +1,44 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const StyledForm = styled.div`
-    margin: 0.5rem 0;
+// const StyledForm = styled.div`
+//     margin: 0.5rem 0;
 
-    & label {
-        font-weight: bold;
-        display: block;
-        margin-bottom: 0.5rem;
-        color: ${(props) => (props.invalid ? "red" : "black")};
-    }
+//     & label {
+//         font-weight: bold;
+//         display: block;
+//         margin-bottom: 0.5rem;
+//         color: ${(props) => (props.invalid ? "red" : "black")};
+//     }
 
-    & input {
-        display: block;
-        width: 100%;
-        border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-        background-color: ${(props) =>
-            props.invalid ? "#ffdbdb" : "transparent"};
-        font: inherit;
-        line-height: 1.5rem;
-        padding: 0 0.25rem;
-    }
+//     & input {
+//         display: block;
+//         width: 100%;
+//         border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//         background-color: ${(props) =>
+//             props.invalid ? "#ffdbdb" : "transparent"};
+//         font: inherit;
+//         line-height: 1.5rem;
+//         padding: 0 0.25rem;
+//     }
 
-    & input:focus {
-        outline: none;
-        background: #ffffff;
-        border-color: #000000;
-    }
+//     & input:focus {
+//         outline: none;
+//         background: #ffffff;
+//         border-color: #000000;
+//     }
 
-    .form-control.invalid input {
-        border-color: red;
-    }
+//     .form-control.invalid input {
+//         border-color: red;
+//     }
 
-    .form-control.invalid label {
-        color: red;
-    }
-`;
+//     .form-control.invalid label {
+//         color: red;
+//     }
+// `;
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
@@ -63,10 +63,10 @@ const CourseInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <StyledForm invalid={!isValid}>
+            <div className={`${styles["form-control"]} ${!isValid && styles.invalid}`}>
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler} />
-            </StyledForm>
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
